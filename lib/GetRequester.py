551ea665c1971e083621869934b6889e10764028
KomfortKimko <komfortkimk@gmail.com>
2024-10-06 18:41:29 +0300

    Getting-remote-data-lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,9 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url) #sends a GET request to the URL
+        return response.content # returs the response body as raw bytes
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body() # retrieves the response body as raw bytes
+        return json.loads(response_body) # Parses the raw byte content as JSON and returns it
